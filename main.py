6cbc18c916aafc3c9e6d4f05902e73550e035068
jimmy <jimmyho.929@outlook.com>
2022-02-11 10:45:17 -0500

    test

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-#initial creation
-import pandas
+import pandas as pd
+
+stats = pd.read_csv('dataset/nba_team_stats_00_to_21.csv')
+print(pd.DataFrame(stats, columns=['WIN%','FGA','FGM','3PA','3PM','SEASON']))
