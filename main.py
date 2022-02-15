952fe25af81d9b9d6564b1a908f029ae2888ee51
jimmy <jimmyho.929@outlook.com>
2022-02-14 22:45:04 -0600

    created scatter plot

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-import pandas
+from os import stat
+import pandas as pd
 from pandasql import sqldf
+import numpy as np
+from sqlalchemy import true
+import matplotlib.pyplot as plt
 
 pysqldf = lambda q: sqldf(q, globals())
 
-stats = pandas.read_csv('dataset/nba_team_stats_00_to_21.csv')
+stats = pd.read_csv('dataset/nba_team_stats_00_to_21.csv')
+#print(pandas.DataFrame(stats, columns=['_3PA','TEAM', 'SEASON']))
+#print(stats[['_3PA','TEAM']])
+#print(stats[(stats['SEASON']=='2000-01')]['_3PA'])
+_20003PAavg = stats.groupby('SEASON')['_3PA'].aggregate(np.mean)
+df_3PA = pd.DataFrame(_20003PAavg)
+df_3PA.reset_index(inplace=True)
+df_3PA.columns=['SEASON', '3PA']
+print(df_3PA)
+df_3PA.plot(x='SEASON', y='3PA', kind='scatter')
+plt.show()
+
 
 q = """SELECT AVG(_3PA)
         FROM stats
@@ -11,4 +26,5 @@ q = """SELECT AVG(_3PA)
         """
 
 _20003PA = pysqldf(q)
-print('2000-01 NBA 3 Point Attempt Average:',float(_20003PA.values))
+#
+#print('2000-01 NBA 3 Point Attempt Average:',float(_20003PA.values))
