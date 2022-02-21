d799e04c1a46111e9484638e8001b97055e31eb3
jimmy <jimmyho.929@outlook.com>
2022-02-21 16:38:16 -0600

    add regex

--- attempt_percentage.py
+++ attempt_percentage.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
-season = input('Which season would you like to see the percentage of 3pt attempts compared to total shot attempts? (ex: 2000-01) ')
+season = input('From the 2000 to 2021 season, which season would you like to see the percentage of 3pt attempts compared to total shot attempts? (ex: 2000-01) ')
+pattern = re.compile('^20[0-2][0-9]\-[0-2][0-9]')
 
-# import dataset with specific columns
-stats_df = pd.read_csv('dataset/nba_team_stats_00_to_21.csv', usecols=['TEAM', '_3PA', 'FGA', 'SEASON'])
+if pattern.match(season):
+    season = season
 
-# select series
-series_3PA = stats_df['_3PA']
-series_FGA = stats_df['FGA']
+    # import dataset with specific columns
+    stats_df = pd.read_csv('dataset/nba_team_stats_00_to_21.csv', usecols=['TEAM', '_3PA', 'FGA', 'SEASON'])
 
-# create new series
-stats_df = stats_df.assign(Threes_Att_Pcnt = (series_3PA / series_FGA) * 100)
-stats_df = stats_df.assign(Twos_Att_Pcnt = 100 - stats_df['Threes_Att_Pcnt'])
+    # select series
+    series_3PA = stats_df['_3PA']
+    series_FGA = stats_df['FGA']
 
-# filter dataframe by user input
-df_shot_attempts = stats_df.loc[stats_df['SEASON'] == season, :]
+    # create new series
+    stats_df = stats_df.assign(Threes_Att_Pcnt = (series_3PA / series_FGA) * 100)
+    stats_df = stats_df.assign(Twos_Att_Pcnt = 100 - stats_df['Threes_Att_Pcnt'])
 
-# create new dataframe 
-df_shot_attempts = pd.DataFrame(df_shot_attempts, columns=['TEAM', 'Threes_Att_Pcnt', 'Twos_Att_Pcnt'])
+    # filter dataframe by user input
+    df_shot_attempts = stats_df.loc[stats_df['SEASON'] == season, :]
 
-df_shot_attempts.plot.bar(x='TEAM', stacked=True, title=f'3pt Attempts Vs. 2pt Attempts {season} Season')
-plt.show()
+    # create new dataframe 
+    df_shot_attempts = pd.DataFrame(df_shot_attempts, columns=['TEAM', 'Threes_Att_Pcnt', 'Twos_Att_Pcnt'])
+
+    df_shot_attempts.plot.bar(x='TEAM', stacked=True, title=f'3pt Attempts Vs. 2pt Attempts {season} Season')
+    plt.show()
+else:
+    print(f'{season} was entered incorrectly. Try again.')
+
+
 
